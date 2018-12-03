026c4a25b0df5d07a39a31b624022a7bf3f0c431
3vikram <viku.vikram05@gmail.com>
2018-12-03 23:15:13 +0530

    Create Delete DB.py

---
+++ Python/SchoolProject/Delete DB.py
@@ -0,0 +1,4 @@
+db_connection = mysql.connector.connect(user='root', password='root',
+                                        host='127.0.0.1', database='School')
+mycursor = db_connection.cursor()
+mycursor.execute("DROP TABLE Teachers")
