546df7c61d2d498a3ca5a084e8bbecc3dcc096fb
3vikram <viku.vikram05@gmail.com>
2018-12-03 22:49:16 +0530

    Update AWS Account Parsing.py

--- AWS Account Parsing.py
+++ AWS Account Parsing.py
@@ -1,66 +1,160 @@
 import re
-from pprint import pprint
-
-class AwsAccountLeadData:
-    def format_file(self, file_path):
-        self.file_path = file_path
-        try:
-            with open(self.file_path, 'r+') as file_read:
-                with open('buffer.txt', 'w+') as file_write:
-
-                    self.total_line_count = 0
-                    for writeline in file_read.readlines():
-                        if not writeline.isspace():
-                            file_write.write(writeline)
-                            self.total_line_count += 1
-        except FileNotFoundError:
-            print("File Not Found")
+import json
 
 
-    def parse_aws_accountnumber(self):
-        try:
-            with open('buffer.txt', 'r+') as file_write:
-                self.file_lines = file_write.readlines()
-                self.aws_account_number = []
-                self.aws_account_number_line_number = []
-                self.final_result = {}
-        except FileNotFoundError:
-            print("File Not Found")
+class AWS_Account_Leads_Parser():
 
-        for readfile in range(len(self.file_lines)):
-            self.line = self.file_lines[readfile]
-            if re.search('AWS_LEADS\[\d[0-9]+\]', self.line):
-                self.aws_account_number.append(self.line.strip().strip('AWS_LEADS[').strip(']'))
-                self.aws_account_number_line_number.append(readfile)
-
+    def __init__(self, file_path):
+        self.path = file_path
+        self.aws_id = []
+        self.aws_id_index = []
+        self.aws_index_length = 0
+        self.final_format = {}
+        self.temp_dict = {}
+        self.temp_aws_id_index = 0
         self.counter = 0
-        self.result = ''
 
-        while(True):
-            if self.counter < len(self.aws_account_number_line_number) - 1:
-                for i in range(self.aws_account_number_line_number[self.counter], self.aws_account_number_line_number[self.counter + 1] - 5):
-                    self.result += self.file_lines[i + 1].strip()
-                self.final_result[self.aws_account_number[self.counter]]= self.result
-                self.counter += 1
-                self.result = ''
-            elif self.counter == len(self.aws_account_number_line_number) - 1:
-                for j in range(self.aws_account_number_line_number[self.counter], self.total_line_count - 1):
-                    self.result += self.file_lines[j + 1].strip().strip('#')
-                    self.final_result[self.aws_account_number[self.counter]]= self.result
-                break
+    def get_aws_account_id(self):
 
-        pretty_printed = pprint(self.final_result)
-        print(pretty_printed)
-
-if __name__ == "__main__":
-    parse = AwsAccountLeadData()
-    file_path = input("Enter the .txt file location to parse AWS Account Lead information: "+ '\n')
-    parse.format_file(file_path)
-    parse.parse_aws_accountnumber()
+        with open(self.path, 'r') as file_read:
+            self.lines_read = file_read.readlines()
 
 
+        for self.line in range(len(self.lines_read)):
+            if re.search('AWS_LEADS\[\d[0-9]+\]', self.lines_read[self.line]):
+                self.aws_id.append(self.lines_read[self.line].replace('AWS_LEADS[', '').replace(']', '').strip('\n'))
+                self.aws_id_index.append(self.line+4)
 
+        return self.aws_id
 
+    def get_aws_leads(self):
 
+        with open(self.path, 'r') as file_read:
+            self.line_read = file_read.readlines()
+            self.aws_id_index_length = len(self.line_read)
 
+        self.aws_index_length = len(self.aws_id_index)
 
+        for index in range(self.aws_index_length):
+            list = []
+
+            if index < self.aws_index_length-1:
+
+                for value in range(self.aws_id_index[index], self.aws_id_index[index+1]-8):
+                    line = self.line_read[value].replace(",", '').strip()
+                    if "uid" in line:
+                        key = line[:5].replace('"', "")
+                        value = line[7:].replace('"', "")
+                        self.temp_dict[key] = value
+                        continue
+                    elif "cn" in line:
+                        key = line[:4].replace('"', "")
+                        value = line[6:].replace('"', "")
+                        self.temp_dict[key] = value
+                        continue
+                    elif "mail" in line:
+                        key = line[:6].replace('"', "")
+                        value = line[8:].replace('"', "")
+                        self.temp_dict[key] = value
+                        continue
+                    elif "corpId" in line:
+                        key = line[:8].replace('"', "")
+                        value = int(float(line[10:].strip('"')))
+                        self.temp_dict[key] = value
+                    else:
+                        continue
+                    list.append(self.temp_dict)
+                    self.temp_dict = {}
+
+            elif index == self.aws_index_length - 1:
+                for value in range(self.aws_id_index[index], self.aws_id_index_length-1):
+                    line = self.line_read[value].replace(",", '').strip()
+                    if "uid" in line:
+                        key = line[:5].replace('"', "")
+                        value = line[7:].replace('"', "")
+                        self.temp_dict[key] = value
+                        continue
+                    elif "cn" in line:
+                        key = line[:4].replace('"', "")
+                        value = line[6:].replace('"', "")
+                        self.temp_dict[key] = value
+                        continue
+                    elif "mail" in line:
+                        key = line[:6].replace('"', "")
+                        value = line[8:].replace('"', "")
+                        self.temp_dict[key] = value
+                        continue
+                    elif "corpId" in line:
+                        key = line[:8].replace('"', "")
+                        value = int(float(line[10:].strip('"')))
+                        self.temp_dict[key] = value
+                    else:
+                        continue
+                    list.append(self.temp_dict)
+                    self.temp_dict = {}
+
+            self.temp_aws_id_index = self.counter
+            self.aws_account_index = self.aws_id[self.temp_aws_id_index]
+            self.final_format[self.aws_account_index] = list
+            self.counter += 1
+
+    def remove_aws_users(self):
+        for key, values in self.final_format.items():
+            for item2 in values:
+                for key1 in list(item2.keys()):
+                    try:
+                        if any(item2[key1] == item for item in self.aws_users_filter_list()):
+                            del item2[key1]
+                            continue
+                        elif item2[key1] == "corpId" and any(item2[key1] == item for item in self.aws_users_filter_list()):
+                            del item2[key1]
+                    except ValueError and TypeError and KeyError:
+                        print("The value trying to pass in filter_aws_users method is not string")
+                    except RuntimeError:
+                        print("Dictionary changed size during iteration")
+
+                    length_aws_key = len(self.final_format[key])
+
+                    for key_index in range(length_aws_key):
+                        if not bool(self.final_format[key][0]):
+                            del self.final_format[key][key_index]
+
+        #print(self.final_format)
+
+    def aws_users_filter_list(self):
+
+        with open('remove_users.txt', 'r') as users_list:
+            self.filter_list = users_list.readlines()
+            filter_list_index = 0
+            for line in self.filter_list:
+                self.filter_list[filter_list_index] = line.strip('\n')
+                filter_list_index += 1
+
+            indexing = 0
+            for filter_list in self.filter_list:
+                try:
+                    convert_to_num = int(filter_list)
+                    if convert_to_num > 0:
+                        self.filter_list[indexing] = convert_to_num
+                        indexing += 1
+                        continue
+                    else:
+                        indexing += 1
+
+                except ValueError:
+                    pass
+                finally:
+                        indexing += 1
+            return self.filter_list
+
+    def convert_to_json(self):
+        with open('AWS_Account_Lead.json', 'w') as write_file:
+            json.dump(self.final_format, write_file, indent= 4)
+
+if __name__ == '__main__':
+    path = input("Please enter the .txt file location for parsing AWS Leads: " + '\n')
+    instance = AWS_Account_Leads_Parser(path)
+    instance.get_aws_account_id()
+    instance.get_aws_leads()
+    instance.remove_aws_users()
+    instance.convert_to_json()
