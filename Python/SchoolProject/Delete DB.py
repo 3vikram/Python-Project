a4145cf6c914149b30a32eb0b7f1561ea37bb7dc
3vikram <viku.vikram05@gmail.com>
2018-12-03 23:17:54 +0530

    Update Delete DB.py

--- Python/SchoolProject/Delete DB.py
+++ Python/SchoolProject/Delete DB.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 db_connection = mysql.connector.connect(user='root', password='root',
                                         host='127.0.0.1', database='School')
 mycursor = db_connection.cursor()
